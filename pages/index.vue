1582e40e84572c696969138e94ebd210d203699c
Kazuki0626 <kanchan210626@gmail.com>
2021-08-15 23:19:35 +0900

    topページのコーディング

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <v-parallax
     dark
-    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    src="https://storage.googleapis.com/setouchikakuregaresorts-media/1/2020/11/s1.jpg"
   >
     <v-row align="center" justify="center">
       <v-col class="text-center" cols="12">
         <h1 class="display-1 font-weight-thin mb-4">Ramen App</h1>
         <div class="my-2">
           <nuxt-link to="/search">
-            <v-btn color="primary" dark>START</v-btn>
+            <div class="ramen-start">
+              <v-btn outlined color="indigo">START</v-btn>
+            </div>
           </nuxt-link>
         </div>
       </v-col>
@@ -19,5 +21,23 @@
 <style lang="scss" scoped>
 .v-parallax {
   height: 100% !important;
+}
+.font-weight-thin {
+  color: #000;
+  font-size: 64px !important;
+}
+.ramen-start {
+  padding: 30px 0;
+}
+.v-application a {
+  text-decoration: none;
+}
+.v-btn:not(.v-btn--round).v-size--default {
+  min-width: 200px;
+  height: 45px;
+  border-radius: 50px;
+}
+.v-application .indigo--text {
+  color: #000 !important;
 }
 </style>
