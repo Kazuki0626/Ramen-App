04a32b64ef45ef4924ea25831d8addf3d93216a1
杉本　諒 <wasp2123321@gmail.com>
2021-10-16 10:52:54 +0900

    back

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -157,6 +157,9 @@ export default {
       longitude: 0,
       show: false,
       freeWord: '',
+      items_1: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+      items_2: ['aaa', 'bbb', 'ccc', 'ddd'],
+      items_3: ['eee', 'fff', 'ggg', 'hhh'],
     }
   },
   mounted() {
@@ -178,8 +181,19 @@ export default {
       .catch(this.setError)
   },
   methods: {
-    doSearch(shop) {
-      this.$store.commit('fetchAllShops', shop)
+    doSearch() {
+      this.$axios
+        .get(
+          `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_URL}&keyword=${this.freeWord}`,
+          {
+            params: {
+              format: 'json',
+            },
+          }
+        )
+        .then((data) => {
+          this.shops = data.data.results.shop
+        })
     },
     setError(err) {
       console.log(err)
